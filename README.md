1afac2fe402dbe7693d0ef3e6d2ae1788ad13dcc
MarcusLages <marcusvlages@gmail.com>
2025-11-26 10:59:41 -0800

    Initial commit with example echo server

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat GO
+Responsive and concurrent chat system using GO coroutines and channels.
+
+## Steps for Execution
+#TODO
---
+++ echo/client.go
@@ -0,0 +1,33 @@
+// echo client
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "net"
+  "os"
+)
+
+func main() {
+  conn, err := net.Dial("tcp", "localhost:6666")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer conn.Close()
+
+  stdinScanner := bufio.NewScanner(os.Stdin)
+  connScanner := bufio.NewScanner(conn)
+
+  for {
+    fmt.Printf("> ")
+    if !stdinScanner.Scan() {
+      break
+    }
+    fmt.Fprintf(conn, "%s\n", stdinScanner.Text())
+    if !connScanner.Scan() {
+      break
+    }
+    fmt.Println(connScanner.Text())
+  }
+}
